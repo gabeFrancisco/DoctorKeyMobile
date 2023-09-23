39a8a3de3d821af5b7d75b3a43bcc85215fc6fe8
gabeFrancisco <gabegit2@gmail.com>
2023-09-23 19:08:53 -0300

    init view buttons and form styles

--- lib/views/init_view.dart
+++ lib/views/init_view.dart
@@ -47,6 +47,7 @@ class _LoginScreenState extends State<InitScreen> {
                     Container(
                       padding: const EdgeInsets.all(30),
                       child: TextFormField(
+                        obscureText: true,
                         style: const TextStyle(color: Colors.white),
                         cursorColor: Colors.white,
                         decoration: const InputDecoration(
@@ -56,25 +57,58 @@ class _LoginScreenState extends State<InitScreen> {
                                 borderSide: BorderSide(color: Colors.white)),
                             border: UnderlineInputBorder(
                                 borderSide: BorderSide(color: Colors.white)),
-                            icon: Icon(Icons.password,
-                                color: Colors.white, size: 38),
+                            icon:
+                                Icon(Icons.key, color: Colors.white, size: 38),
                             focusColor: Colors.white,
-                            labelText: "Usuário",
+                            labelText: "Senha",
                             labelStyle: TextStyle(color: Colors.white)),
                       ),
                     ),
                     Container(
-                        padding: const EdgeInsets.all(30),
-                        child: TextButton(
-                            onPressed: () {},
-                            child: const Text("Entrar"),
-                            style: ButtonStyle(
-                                foregroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        Colors.white),
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        Colors.green.shade300))))
+                      margin: const EdgeInsets.all(10),
+                      child: TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.green,
+                            backgroundColor: Colors.white,
+                            minimumSize: const Size(150, 48),
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                          ),
+                          child: const Text(
+                            "Entrar!",
+                            style: TextStyle(fontSize: 20),
+                          )),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Esqueci minha senha",
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Não é um usuário? ",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            TextButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  "Cadastre-se aqui!",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    decorationColor: Colors.white,
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                )),
+                          ]),
+                    )
                   ],
                 ))
           ]),
